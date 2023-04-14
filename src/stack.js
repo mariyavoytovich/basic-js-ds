068a31f4f1e1035e4b04b4727b5493e4b4795176
Maryia.Vaitovich <Maryia.Vaitovich@WSA-050-73>
2023-04-14 09:16:27 +0300

    feat: add stack implementation

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  _values = [];
+
+  push(element) {
+    this._values.push(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this._values.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this._values[this._values.length -1 ];
   }
 }
 
